0496a276af0e65cc2e3f78c831e4c58835dbc702
Ilya <krosh2507@gmail.com>
2020-06-24 23:37:13 +0300

    Updates

---
+++ precache-manifest.3fe8eecd013a879ee82f0f9bf67dd8ed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ca9fa8d7a5ac2d91f5617aa1ba4a60",
+    "url": "/fulogy-app/index.html"
+  },
+  {
+    "revision": "1a42c309dee470b75ef0",
+    "url": "/fulogy-app/static/css/2.382c35bb.chunk.css"
+  },
+  {
+    "revision": "ad99405c237f12b3d00c",
+    "url": "/fulogy-app/static/css/main.be766aca.chunk.css"
+  },
+  {
+    "revision": "1a42c309dee470b75ef0",
+    "url": "/fulogy-app/static/js/2.22c4ff6a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fulogy-app/static/js/2.22c4ff6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad99405c237f12b3d00c",
+    "url": "/fulogy-app/static/js/main.7b45a2c9.chunk.js"
+  },
+  {
+    "revision": "357ddc008a2fe6fac9a3",
+    "url": "/fulogy-app/static/js/runtime-main.e2a6aca2.js"
+  },
+  {
+    "revision": "7509c8ef0174eed8bc3ec615c9dbe787",
+    "url": "/fulogy-app/static/media/arrow-icon.7509c8ef.svg"
+  },
+  {
+    "revision": "9c231634dd7d0bb7a6ae1203bc7ca07c",
+    "url": "/fulogy-app/static/media/check-square-solid.9c231634.svg"
+  },
+  {
+    "revision": "17861ffead670d4056c589854f7e795c",
+    "url": "/fulogy-app/static/media/info-solid.17861ffe.svg"
+  },
+  {
+    "revision": "8ed1c4228540e1370b90a01d7ffef60d",
+    "url": "/fulogy-app/static/media/logo.8ed1c422.svg"
+  },
+  {
+    "revision": "cc463b2a17df634edd5dcda60c8f3577",
+    "url": "/fulogy-app/static/media/shopping-basket-solid.cc463b2a.svg"
+  }
+]);
